624244c5274474a89d074e730ffacf0fa9ac49fc
Éva <eva.mery@etu.unilim.fr>
2021-10-24 18:52:38 +0200

    Ajout des vidéos !!

--- src/Controller/Frontoffice/EMTeaRoomController.php
+++ src/Controller/Frontoffice/EMTeaRoomController.php
@@ -5,14 +5,28 @@ namespace App\Controller\Frontoffice;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Video;
 
 class EMTeaRoomController extends AbstractController
 {
     #[Route('/tea-room', name: 'e_m_tea_room')]
     public function index(): Response
     {
+
+        $em = $this->getDoctrine()->getManager();
+        $videos = $em->getRepository(Video::class)->findAll();
+
+        $tabURL = [];
+        $URL = ["",""];
+        foreach ($videos as $video) {
+            $URL = explode('=', $video->getLien());
+            $tabURL[]=$URL[1];
+            //$tabURL->array_push($URL[1]);
+        }
+
         return $this->render('e_m_tea_room/index.html.twig', [
-            'controller_name' => 'EMTeaRoomController',
+            'tabURL' => $tabURL,
+            'videos' => $videos,
         ]);
     }
 }
